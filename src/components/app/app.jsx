68c5433fda9bd597f81006b3acffae86244f1909
kyledarrington <kyle@kyledarrington.com>
2019-12-27 23:31:15 -0500

    small reformat of app.jsx

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -30,9 +30,9 @@ export default class App extends Component {
         return (
             <div>
                 <ModalContext.Provider value={this.state}>
-                <NavbarContainer />
+                    <NavbarContainer />
                     <ModalContainer />
-                <Home />
+                    <Home />
                 </ModalContext.Provider>
             </div>
         )
