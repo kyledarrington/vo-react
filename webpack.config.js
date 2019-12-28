d0a135942aff8c69b01e3b7a548dfb290fb665b4
kyledarrington <kyle@kyledarrington.com>
2019-12-27 23:14:22 -0500

    Update Webpack Config to include Blog entry point

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,64 @@
 const path = require('path')
 
-module.exports = {
-    entry: "./src/index.jsx",
-    module: {
-        rules: [
-            {
-                test: /\.(js)|\.(jsx)$/,
-                exclude: /node-modules/,
-                use: ['babel-loader']
-            },
-            {
-                test: /\.(scss)$/,
-                use: ['style-loader', 'css-loader', 'sass-loader']
-            }
-        ]
+module.exports = [
+    {
+        entry: {
+            main: "./src/index.jsx",
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.(js)$|\.(jsx)$/,
+                    exclude: /node-modules/,
+                    use: ['babel-loader']
+                },
+                {
+                    test: /\.(scss)$/,
+                    use: ['style-loader', 'css-loader', 'sass-loader']
+                }
+            ]
+        },
+        resolve: {
+            extensions: ['*', '.js', '.jsx', '.json']
+        },
+        output: {
+            path: path.resolve(__dirname, 'public'),
+            publicPath: "public/",
+            filename: "[name].bundle.js"
+        },
+        devServer: {
+            contentBase: path.join(__dirname, 'public'),
+            historyApiFallback: true
+        }
     },
-    resolve: {
-        extensions: ['*', '.js', '.jsx']
-    },
-    output: {
-        path: path.resolve(__dirname, 'public'),
-        publicPath: "public/",
-        filename: "bundle.js"
-    },
-    devServer: {
-        contentBase: './public',
-        historyApiFallback: true
+    {
+        entry: {
+            blog: "./src/blog/index.jsx",
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.(js)$|\.(jsx)$/,
+                    exclude: /node-modules/,
+                    use: ['babel-loader']
+                },
+                {
+                    test: /\.(scss)$/,
+                    use: ['style-loader', 'css-loader', 'sass-loader']
+                }
+            ]
+        },
+        resolve: {
+            extensions: ['*', '.js', '.jsx', '.json']
+        },
+        output: {
+            path: path.resolve(__dirname, 'public/blog'),
+            publicPath: path.join(__dirname, 'public'),
+            filename: "[name].bundle.js"
+        },
+        devServer: {
+            contentBase: path.join(__dirname, 'public'),
+            historyApiFallback: true
+        }
     }
-}
+]
