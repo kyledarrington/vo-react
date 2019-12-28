ce378c7385472617b8d94af03f80be1099782f7e
kyledarrington <kyle@kyledarrington.com>
2019-12-28 12:04:36 -0500

    Improve responsiveness of article component

--- src/blog/components/article/article.jsx
+++ src/blog/components/article/article.jsx
@@ -10,7 +10,7 @@ export default function Article(props) {
             <div className="article-content">
                 <h1 className="article-title">{props.data.title}</h1> 
                 <p><em>Posted {postDate}</em></p>
-                <img src={props.data.headerSrc} />
+                <img className="article-header-image" src={props.data.headerSrc} />
                 <ReactMarkdown source={props.data.mdBody} />
             </div>
         </div>
