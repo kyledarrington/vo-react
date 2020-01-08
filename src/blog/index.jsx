8fbd4d36c2526f0b20e74d4c1a89cd150222ad9b
kyledarrington <kyle@kyledarrington.com>
2020-01-07 22:04:01 -0500

    Remove debug tags

--- src/blog/index.jsx
+++ src/blog/index.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom'
 import App from './components/app/app.jsx'
 import ReactGA from 'react-ga';
 
-ReactGA.initialize('UA-138493704-2', {debug:true});
+ReactGA.initialize('UA-138493704-2');
 ReactGA.pageview(window.location.pathname + window.location.search);
 
 ReactDOM.render(<App />, document.getElementById('root'))
