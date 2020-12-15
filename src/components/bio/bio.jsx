681ff6199cf238a05cabae3d41766f8589839dec
Kyle Arrington <kyle.arrington@borisfx.com>
2020-12-14 23:17:28 -0500

    New Components - Bio and Studio

---
+++ src/components/bio/bio.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import './style.scss';
+
+export default function Bio() {
+    return (
+        <div>
+            <p>
+                My name is Kyle D. Arrington, and I’ve been talking into
+                microphones from the moment I knew it was possible. With over a
+                decade of performance experience and an intuitive understanding
+                of storytelling, I know how to make your words resonate with
+                your audience. As a Voice Actor, I strive to give an authentic,
+                bold performance that people can truly connect to. As a client,
+                I want you to feel your needs have been exceeded, and your time
+                has been respected. In short: I deliver results, and I deliver
+                them fast. Off mic, I’m an avid gamer and an all around nerd.
+                I’m the type to bounce between hobbies, from coding to baking to
+                learning new languages. 
+            </p>
+        </div>
+    );
+}
---
+++ src/components/studio/studio.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import {Mic, Speaker, Wifi} from 'react-bootstrap-icons'
+import './style.scss'
+
+export default function Studio() {
+    return (
+        <div class="container justify-content-center branded-circle">
+            <h2>Home Studio</h2>
+            <div class="row w-100 mt-4">
+                <div class="col-2 offset-1"> <Mic color="#ee1302" size={48} /> </div>
+                <div class="col-auto"><h4 class="d-inline">CAD E-100S</h4></div>
+            </div>
+            <div class="row w-100 mt-4">
+                <div class="col-2 offset-1"> <Speaker color="#ee1302" size={48} /></div>
+                <div class="col-auto"> <h4 class="d-inline">Focusrite Scarlett Solo</h4></div>
+            </div>
+            <div class="row w-100 mt-4">
+                <div class="col-2 offset-1"> <Wifi color="#ee1302" size={48} /></div>
+                <div class="col-auto"> <h4 class="d-inline">Source-Connect Enabled</h4></div>
+            </div>
+        </div>
+    )
+}
