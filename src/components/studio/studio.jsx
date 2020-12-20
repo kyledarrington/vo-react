f64f377f73cf31dd968d11b649a612bac5bfdcf9
Kyle Arrington <kyle@kyledarrington.com>
2020-12-19 23:55:07 -0500

    Update studio component

--- src/components/studio/studio.jsx
+++ src/components/studio/studio.jsx
@@ -4,7 +4,7 @@ import './style.scss'
 
 export default function Studio() {
     return (
-        <div class="container justify-content-center branded-circle">
+        <div class="container justify-content-center text-center branded-box py-3">
             <h2>Home Studio</h2>
             <div class="row w-100 mt-4">
                 <div class="col-2 offset-1"> <Mic color="#ee1302" size={48} /> </div>
@@ -12,7 +12,7 @@ export default function Studio() {
             </div>
             <div class="row w-100 mt-4">
                 <div class="col-2 offset-1"> <Speaker color="#ee1302" size={48} /></div>
-                <div class="col-auto"> <h4 class="d-inline">Focusrite Scarlett Solo</h4></div>
+                <div class="col-auto"> <h4 class="d-inline">Audient iD14</h4></div>
             </div>
             <div class="row w-100 mt-4">
                 <div class="col-2 offset-1"> <Wifi color="#ee1302" size={48} /></div>
