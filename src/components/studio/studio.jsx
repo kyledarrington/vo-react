cf3fd29696f7c01d047e10bdac01f7016770f0e4
Kyle Arrington <kyle@kyledarrington.com>
2021-01-09 12:03:40 -0500

    change studio mic

--- src/components/studio/studio.jsx
+++ src/components/studio/studio.jsx
@@ -8,7 +8,7 @@ export default function Studio() {
             <h2>Home Studio</h2>
             <div class="row w-100 mt-4">
                 <div class="col-2 offset-1"> <Mic color="#ee1302" size={48} /> </div>
-                <div class="col-auto"><h4 class="d-inline">CAD E-100S</h4></div>
+                <div class="col-auto"><h4 class="d-inline">Sennheiser MKH 416</h4></div>
             </div>
             <div class="row w-100 mt-4">
                 <div class="col-2 offset-1"> <Speaker color="#ee1302" size={48} /></div>
